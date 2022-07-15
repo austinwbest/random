b331dd1d24cd6d4d175658ed4bc87f47cb106600
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2022-07-14 20:26:22 -0500

    fixup!

--- language.php
+++ language.php
@@ -123,7 +123,7 @@ $searchFor = array(
   array(
     'apply' => ((in_array('regex_tagDiv', $regexs)) ? true : false),
     'name' => 'tagDiv',
-    'regex' => '#<div[^>]*>([^&][\w\s;&]+)</div>#s'
+    'regex' => '#<div[^>]*>('[\w\W]+)'</div>#s'
   ),
   array(
     'apply' => ((in_array('regex_defineQuote', $regexs)) ? true : false),
