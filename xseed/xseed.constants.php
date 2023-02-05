c6555afe1135af6ffc88b16a7a588c2eaaab40a9
nitsua <austin.w.best@gmail.com>
2023-02-04 22:20:42 -0500

    Update xseed.constants.php

--- xseed/xseed.constants.php
+++ xseed/xseed.constants.php
@@ -1,4 +1,5 @@
-<?PHP
+<?php
+
 /*
 ----------------------------------
  ------  Created: 042321   ------
@@ -6,7 +7,7 @@
 ----------------------------------
 */
 
-define('HISTORY', 50); //-- GRAB THE NEWEST AMOUNT OF ITEMS ADDED TO QBT EACH RUN, SINCE IT RUNS ON A CRON THIS DOES NOT NEED TO BE A LOT
+define('HISTORY', 15); //-- GRAB THE NEWEST AMOUNT OF ITEMS ADDED TO QBT EACH RUN, SINCE IT RUNS ON A CRON THIS DOES NOT NEED TO BE A LOT
 
 define('JACKET_URL', 'http://localhost:9117');
 define('JACKETT_API', '');
@@ -16,10 +17,8 @@ define('QBT_USER', 'admin');
 define('QBT_PASS', '');
 define('QBT_TAG', 'xseed');
 
-//-- THE NAME IN JACKETT
-$xseedIndexers = array('BHD', 
-					   'DC',
-					   'IPT',
-					   'TL'
+$xseedIndexers = array('beyond-hd', 
+                       'hdbitsapi',
+                       'hdtorrents'
 					  );
 ?>
