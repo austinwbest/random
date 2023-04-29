6f7e3c06f6e6128ce545aad7933205e216e318a3
bakerboy448 <55419169+bakerboy448@users.noreply.github.com>
2023-04-29 15:14:31 -0500

    Update language.php - add trx support (typescript)

--- language.php
+++ language.php
@@ -142,7 +142,7 @@ $searchFor = array(
   ),
 );
 
-$searchFiles = array('js');
+$searchFiles = array('js', 'trx');
 
 ?>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
