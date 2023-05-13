c9b96d57f8edd5cf30b5989008cdb515adeeb515
nitsua <austin.w.best@gmail.com>
2023-05-13 19:32:42 -0400

    Update trackerErrors.py
    Updated to only send when all urls are in an error state and show how many it is sending

--- trackerErrors.py
+++ trackerErrors.py
@@ -1,13 +1,17 @@
+# REQUIRED: pip install qbittorrent-api
 import qbittorrentapi
 import subprocess
 import json
+import time
 from urllib.parse import urlparse
 
+NOTIFIARR_PY = 'C:\\Users\\nitsua\\Desktop\\scripts\\notifiarr.py'
+
 qbt_client = qbittorrentapi.Client(
-    host='localhost',
-    port=8080,
-    username='',
-    password='',
+    host = 'localhost',
+    port = 8080,
+    username = '',
+    password = ''
 )
 
 try:
@@ -24,22 +28,31 @@ for torrent in qbt_client.torrents_info():
             torrentObject = {k: v for k, v in torrent.items()}
             torrentObject.pop('magnet_uri', None)
             torrentObject.pop('content_path', None)
-            torrentObject['tracker'] = torrentObject['tracker'].rsplit('/', 1)[0]
 
-            realTrackers = []
+            workingURL      = ''
+            brokenURL       = ''
+            brokenMsg       = ''
+            realTrackers    = []
             realTrackerItem = {}
+
+            # Check all urls, ignore if at least one works
             for trackerItem in trackers:
-                if '*' not in trackerItem['url'] and trackerItem['msg']:
-                    realTrackerItem['url'] = trackerItem['url'].rsplit('/', 1)[0]
-                    realTrackerItem['msg'] = trackerItem['msg']
-                    realTrackerItem['num_peers'] = trackerItem['msg']
-                    realTrackers.append(realTrackerItem)
+                if '*' not in trackerItem.url:
+                    if not trackerItem.msg:
+                        workingURL = trackerItem.url
+                    else:
+                        brokenURL = trackerItem.url
+                        brokenMsg = trackerItem.msg
 
-            errors[torrent.hash[-6:]] = {'torrent': torrentObject, 'trackers': realTrackers}
-            break
+            if not workingURL and brokenURL:
+                realTrackerItem['url'] = brokenURL.rsplit('/', 1)[0]
+                realTrackerItem['msg'] = brokenMsg
+                realTrackers.append(realTrackerItem)
+                errors[torrent.hash[-6:]] = {'torrent': torrentObject, 'trackers': realTrackers}
 
+counter = 1
+totalErrors = len(errors)
 for hash, item in errors.items():
-    print(item)
     inlineFields    = []
     inlineField     = {}
     singleFields    = []
@@ -55,12 +68,12 @@ for hash, item in errors.items():
 
     singleField['Name'] = item['torrent']['name']
 
-    urls = ''
-    urlErrors = ''
+    urls        = ''
+    urlErrors   = ''
     for tracker in item['trackers']:
         url = urlparse(tracker['url'])
-        urls += url.scheme + '://' + url.netloc + "\n";
-        urlErrors += tracker['msg']
+        urls += url.scheme + '://' + url.netloc + "\n"
+        urlErrors += tracker['msg'] + "\n"
 
     singleField['URL'] = '```' + urls + '```'
     singleField['Error'] = '```' + urlErrors + '```'
@@ -68,5 +81,8 @@ for hash, item in errors.items():
     inlineFields.append(inlineField)
     singleFields.append(singleField)
 
-    webhook = "python notifiarr.py -e \"QBT Tracker Error\" -c 631827062348578827 -m \"FFA500\" -t \"Tracker Error\" -b \"Tracker URLs found with errors\" -g \"" + json.dumps(inlineFields).replace('"', r'\"') + "\" -f \"" + json.dumps(singleFields).replace('"', r'\"') + "\" -a \"https://notifiarr.com/images/logo/notifiarr.png\" -z \"Passthrough Integration\""
+    webhook = "python " + NOTIFIARR_PY + " -e \"QBT Tracker Error\" -c 631827062348578827 -m \"FFA500\" -t \"Tracker Error ("+ str(counter) +"/"+ str(totalErrors) +")\" -b \"Tracker URLs found with errors\" -g \"" + json.dumps(inlineFields).replace('"', r'\"') + "\" -f \"" + json.dumps(singleFields).replace('"', r'\"') + "\" -a \"https://notifiarr.com/images/logo/notifiarr.png\" -z \"Passthrough Integration\""
+    print(webhook)
     subprocess.call(webhook)
+    time.sleep(1)
+    counter = counter + 1
